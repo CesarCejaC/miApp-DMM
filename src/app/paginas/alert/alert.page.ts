28807d090aef11d4e16a9e834d96fb09e1743f51
CesarCejaC <tonycejacorte@gmail.com>
2022-02-25 08:43:04 -0600

    clase viernes 25-2-22

--- src/app/paginas/alert/alert.page.ts
+++ src/app/paginas/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertCrl: AlertController) { }
 
   ngOnInit() {
   }
+
+  async presentAlert(){
+    const alert = await this.alertCrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      subHeader:'Hola alumnos',
+      message:'Espero aprendan algo hoy y la máquna funciona',
+      buttons:['OK']
+    });
+  }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertCrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['Cancel', 'Open Modal', 'Delete']
+    });
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertCrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Encuesta',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        // multiline input.
+        {
+          name: 'paragraph',
+          id: 'paragraph',
+          type: 'textarea',
+          placeholder: 'Placeholder 3'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date',
+          min: '2021-03-01',
+          max: '2023-01-12'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        },
+        {
+          name: 'name8',
+          type: 'password',
+          placeholder: 'Advanced Attributes',
+          cssClass: 'specialClass',
+          attributes: {
+            maxlength: 4,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'rojo',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 
 }
